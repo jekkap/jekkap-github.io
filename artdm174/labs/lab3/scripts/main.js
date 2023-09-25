d6939e989fd6024ba7084ff3a1e61f8351c306e7
Jake <132245616+jekkap@users.noreply.github.com>
2023-09-25 14:31:35 -0700

    Add files via upload

---
+++ artdm174/labs/lab3/scripts/main.js
@@ -0,0 +1,28 @@
+window.addEventListener('mousemove', function (e) {
+    this.document.getElementById('x-value').textContent = e.x;
+    this.document.getElementById('y-value').textContent = e.y;
+});
+
+function changeNight(fileName) {
+    let background = document.querySelector("#day")
+    background.classList.add('night');
+    console.log('night');
+}
+
+function changeDay(fileName) {
+    let background = document.querySelector("#day")
+    background.classList.remove('night');
+    console.log('day');
+}
+
+const heli = document.getElementById('helicopter')
+
+document.addEventListener('click', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const newX = x - heli.width * 2;
+    const newY = y - heli.width * 2;
+
+    heli.style.transform = `translate(${newX}px, ${newY}px)`;
+})
